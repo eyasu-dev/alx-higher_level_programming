728d0ace3833100c02a2a9cb4802b56c491da58d
eyasu sintayehu <79601234+eyasu-dev@users.noreply.github.com>
2023-10-10 01:03:43 +0400

    7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -2,39 +2,17 @@
 """
 Holds the class superclass BaseGeometry
 """
-Base = __import__('6-base_geometry').BaseGeometry
+class BaseGeometry:
+    """An empty class"""
 
-
-class BaseGeometry(Base):
-    """BaseGeometry class containing area and integer_validator
-    """
+    def area(self):
+        """Raise an exception with the specified message"""
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates input passed to match conditions provided
-        """
+        """Validates the value for int type."""
 
         if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
-
-bg = BaseGeometry()
-
-bg.integer_validator("my_int", 12)
-bg.integer_validator("width", 89)
-
-try:
-    bg.integer_validator("name", "John")
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    bg.integer_validator("age", 0)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    bg.integer_validator("distance", -4)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+            raise ValueError("{} must be greater than 0".format(name))
